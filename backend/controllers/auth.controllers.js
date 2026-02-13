c0dbc389fbf92504477352256741a296a6af25ad
camilleingram <camille_d_ingram@live.com>
2026-02-13 13:01:58 -0500

    initial commit

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,11 @@
+
+export const signup =  async (req, res) => {
+    res.send("Signup route is being called");
+}
+export const login =  async (req, res) => {
+    res.send("Login route is being called");
+}
+export const logout =  async (req, res) => {
+    res.send("Logout route is being called");
+}
+
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from "./lib/db.js"
+
+import authRoutes from "./routes/auth.routes.js";
+import { connect } from 'mongoose';
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ht ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
+  connectDB()
+
 });
