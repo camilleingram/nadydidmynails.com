a1f31d2f13ef0970e59009bd567f1471eb5723b0
camilleingram <camille_d_ingram@live.com>
2026-02-27 13:34:29 -0500

    add generate tokens function

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,6 +1,33 @@
+import User from "../models/user.models"
+import redis from "ioredis"
+import jwt from "jsonwebtoken"
+
+
+const generateTokens = async(userId) => {
+    const accessToken = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: 15 * 60})
+
+    const refreshToken = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: 7 * 24 * 60 * 60})
+
+    return {accessToken, refreshToken}
+}
 
 export const signup =  async (req, res) => {
-   res.send("Signup route is being called");
+    const {name, email, password} = req.body
+    try {
+        const foundUser = await User.findOne({email})
+
+        if(!foundUser) {
+            const newUser = User.create({name, email, password})
+            res.status(201).json({message: "User created successfully"})
+        }
+
+        res.status(400).json({message: "User already exists"})
+
+    } catch (error) {
+        res.status(500).json({message: "Error creating new user"})
+    }
+    
+
 }
 export const login =  async (req, res) => {
     res.send("Login route is being called");
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import { signup, login, logout } from "../controllers/auth.controllers.js";
 
+
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
