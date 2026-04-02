6ae2a2b57e245262179e9aa50b4c4713df1e220f
camilleingram <camille_d_ingram@live.com>
2026-04-02 11:28:04 -0400

    finish create product function

--- backend/controllers/product.controllers.js
+++ backend/controllers/product.controllers.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js"
 import Product from "../models/product.model.js"
 import { redis } from "../lib/redis.js"
 import jwt from "jsonwebtoken"
+import cloudinary from "../lib/cloudinary.js"
 
 export const getAllProducts = async (req, res) => {
     try {
@@ -12,9 +13,9 @@ export const getAllProducts = async (req, res) => {
     }
 }
 
-export const getFeaturedProducts = (req, res) => {
+export const getFeaturedProducts = async (req, res) => {
     try {
-        const featured = Product.find({isFeatured: true})
+        const featured = await Product.find({isFeatured: true})
 
         if(featured) {
             return res.status(200)
@@ -24,4 +25,30 @@ export const getFeaturedProducts = (req, res) => {
     } catch (error) {
         res.status(500).json({message: error.message})
     }
+}
+
+export const createProduct = async (req, res) => {
+     try {
+        
+        const {name, description, price, image, category} = req.body
+
+        const cloudinaryResponse = null
+
+        if(image) {
+            await cloudinary.uploader.upload(image, {folder: products})
+        }
+
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
+            category
+        })
+
+        res.status(201).json(product, {message: "Product created successfully"})
+
+     } catch (error) {
+        res.status(500).json({message: "Error with createProduct function"})
+     }
 }
