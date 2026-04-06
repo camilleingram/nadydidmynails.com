794e1d4efc2611f337d63853f3fcf6750d8317d4
camilleingram <camille_d_ingram@live.com>
2026-04-05 20:01:31 -0400

    add add to cart route

--- backend/routes/cart.routes.js
+++ backend/routes/cart.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { protectRoute } from "../middlewares/auth.middlewares.js" 
-import { getCart } from "../controllers/cart.controllers.js"
+import { getCart, addToCart } from "../controllers/cart.controllers.js"
 const router = express.Router()
 
 router.get("/", protectRoute, getCart)
+router.post("/", protectRoute, addToCart)
 
 export default router
