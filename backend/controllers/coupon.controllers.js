bdc5b265ae8080e6c5e4a08cc7ad1e320d73e7f3
camilleingram <camille_d_ingram@live.com>
2026-04-06 10:42:54 -0400

    finish get coupon function

--- backend/controllers/coupon.controllers.js
+++ backend/controllers/coupon.controllers.js
@@ -1,1 +1,19 @@
+import Coupon from "../models/coupon.model.js"
 
+export const getCoupon = async (res, req) => {
+     try {
+        const user = req.user
+
+        const foundCoupon = await Coupon.findOne({userId: user.id, isActive: true})
+
+        if(foundCoupon) {
+            res.status(200).json({message: "Coupon found"}, foundCoupon)
+        }else {
+            res.status(404).json({message: "Coupon not found"}, null)
+        }
+        
+     } catch (error) {
+        console.log("error in getCoupon controller", error.message)
+        res.status(500).json({message: "Server error", error: error.message})
+     }
+}
--- backend/routes/coupon.routes.js
+++ backend/routes/coupon.routes.js
@@ -1,5 +1,9 @@
 import express from "express"
+import { protectRoute } from "../middlewares/auth.middlewares.js"
+import { getCoupon } from "../controllers/coupon.controllers.js"
 
 const router = express.Router()
+
+router.get("/", protectRoute, getCoupon)
 
 export default router
