aedba5f71e55806b4badfab8b9c65499ce40dfbf
camilleingram <camille_d_ingram@live.com>
2026-04-06 12:12:29 -0400

    set up order model

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js"
 import cartRoutes from "./routes/cart.routes.js"
 import couponRoutes from "./routes/coupon.routes.js"
+import paymentRoutes from "./routes/payment.route.js"
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -20,6 +21,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
+app.use("/api/payments", paymentRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
