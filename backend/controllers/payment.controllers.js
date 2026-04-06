cf13a53758632c19dd0fe18aa3d14074a318c927
camilleingram <camille_d_ingram@live.com>
2026-04-06 19:28:15 -0400

    finish create stripe coupon function

---
+++ backend/controllers/payment.controllers.js
@@ -0,0 +1,89 @@
+import Coupon from "../models/coupon.model.js"
+import Order from "../models/order.model.js"
+import Stripe from "../lib/stripe.js"
+
+export const createCheckoutSession = async (req, res) => {
+    try {
+        const { products, couponCode } = req.body
+
+        if(!Array.isArray(products) || products.length === 0) {
+            return res.status(400).json({message: "Products is invalid or empty"})
+        }
+
+        const totalAmount = 0
+
+        const lineItems = products.map((item) => {
+            const amount = item.price * 100
+            totalAmount += amount * item.quantity
+
+            return { 
+                price_data: {
+                    currency: "usd",
+                    product_data: {
+                        name: item.name,
+                        image: [products.image]
+                    },
+                    unit_amount: amount
+                }
+            }
+        })
+
+        const coupon = null
+
+        if(couponCode) {
+            const coupon = await Coupon.findOne({code: couponCode, userId: req.user._id, isActive: true})
+            
+            if(coupon) {
+                totalAmount -= totalAmount * (coupon.discountPercentage / 100)
+                return res.status(200).json({message: "Coupon applied"})
+            }
+        }
+
+        const session = await Stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: lineItems,
+            mode: "payments",
+            success_url: `${process.env.BASE_URL}/success`,
+            return_url: `${process.env.BASE_URL}/return`,
+            discounts: coupon ? [
+                {
+                    coupon: await createStripeCoupon(coupon.discountPercentage)
+                },
+            ] : [],
+            metadata: {
+                userId: req.user._id.toString(),
+                couponCode: couponCode || "",
+                products: JSON.stringify(
+                    products.map((product) => {
+                        return {
+                            id: product._id,
+                            quantity: product.quantity,
+                            price: product.price
+                        }
+                    })
+                )
+            }
+        })
+
+
+    } catch (error) {
+        console.log("Error in createCheckoutSession controller", error,message)
+        res.status(500).json({message: "Server error", error: error.message})
+    }
+}
+
+const createStripeCoupon = async (discountPercentage) => {
+    try {
+        const coupon = await Stripe.coupons.create({
+            percent_off: discountPercentage,
+            duration: "once"
+        })
+
+        res.status(201).json({message: "Coupon created successfully"})
+
+        return coupon._id
+    } catch (error) {
+        console.log("Error in createStripeCoupon helper function", error.message)
+        res.status(500).json({message: "Server error", error: error.message})
+    }
+}
--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,7 +1,9 @@
 import express from "express"
+import { protectRoute } from "../middlewares/auth.middlewares.js"
+import { createCheckoutSession } from "../controllers/payment.controllers.js"
 
 const router = express.Router()
 
-
+router.post("/create-checkout-session", protectRoute, createCheckoutSession)
 
 export default router
