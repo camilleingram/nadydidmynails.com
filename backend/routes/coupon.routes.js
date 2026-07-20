b12b7d661af95b63e33253e364766c273de97674
camilleingram <camille_d_ingram@live.com>
2026-07-20 18:00:41 -0400

    fix coupon routes

--- backend/routes/coupon.routes.js
+++ backend/routes/coupon.routes.js
@@ -1,13 +1,14 @@
 import express from "express"
-import { protectRoute } from "../middlewares/auth.middlewares.js"
-import { getAllCoupons, getOneCoupon, createCoupon, deleteCoupon, updateCoupon } from "../controllers/coupon.controllers.js"
+import { protectRoute, adminRoute } from "../middlewares/auth.middlewares.js"
+import { getAllCoupons, getOneCoupon, createCoupon, deleteCoupon, updateCoupon, validateCoupon } from "../controllers/coupon.controllers.js"
 
 const router = express.Router()
 
-router.get("/", getAllCoupons)
+router.get("/", protectRoute, adminRoute, getAllCoupons)
 router.get("/:couponId", getOneCoupon)
-router.post("/", createCoupon)
-router.delete("/:couponId", deleteCoupon)
-router.put("/:couponId", updateCoupon)
+router.post("/", protectRoute, adminRoute, createCoupon)
+router.delete("/:couponId", protectRoute, adminRoute, deleteCoupon)
+router.put("/:couponId", protectRoute, adminRoute, updateCoupon)
+router.post("/validate/:couponId", protectRoute, adminRoute, validateCoupon)
 
 export default router
