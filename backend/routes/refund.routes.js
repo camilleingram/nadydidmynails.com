d3a3d6a6e8ec4359628d3cdf8b306e512ac915b5
camilleingram <camille_d_ingram@live.com>
2026-07-20 20:10:49 -0400

    fix refund routes

--- backend/routes/refund.routes.js
+++ backend/routes/refund.routes.js
@@ -1,4 +1,4 @@
-import express from "express",
+import express from "express"
 import { protectRoute, adminRoute } from "../middlewares/auth.middlewares.js"
 import { getAllRefunds, getOneRefund, createRefund, deleteRefund } from "../controllers/refund.controllers.js"
 
@@ -7,6 +7,5 @@ const router = express.Router()
 router.get("/", protectRoute, adminRoute, getAllRefunds)
 router.get("/:refundId", protectRoute, adminRoute, getOneRefund)
 router.post("/", protectRoute, createRefund)
-router.delete("/:refundId", protectRoute, adminRoute, deleteRefund)
 
 export default router
