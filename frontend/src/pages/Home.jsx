8bfbb1af3c9bac970ce92104dff3232fc2efffe7
camilleingram <camille_d_ingram@live.com>
2026-07-28 18:50:21 -0400

    set up home page, fonts , and assets

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-   <div className="bg-off-white">
-    <h1>Test</h1>
+   <div className="w-[100vw] h-[100vh] bg-off-white">
+    <div className="h-[5%] bg-burgundy">
+      <h2 className="text-off-white text-[1rem]">GET 10% OFF YOUR PURCHASE USE CODE NDMNLAUNCH</h2>
+    </div>
 
    </div>
   )
