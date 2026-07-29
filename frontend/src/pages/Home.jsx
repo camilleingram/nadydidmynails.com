e5dad94b763ba208270ad4bc399b9a18376894bd
camilleingram <camille_d_ingram@live.com>
2026-07-29 17:15:56 -0400

    add font

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
    <div className="w-[100vw] h-[100vh] bg-off-white">
     <div className="h-[5%] bg-burgundy">
-      <h2 className="text-off-white text-[1rem]">GET 10% OFF YOUR PURCHASE USE CODE NDMNLAUNCH</h2>
+      <h2 className="text-off-white text-[0.75rem] font-[Merchant] font-extralight ">GET 10% OFF YOUR PURCHASE USE CODE NDMNLAUNCH</h2>
     </div>
 
    </div>
