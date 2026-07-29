ced1a1db927ed650c503ddda43b6273c958f1cc0
camilleingram <camille_d_ingram@live.com>
2026-07-29 18:12:39 -0400

    add menu lines

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className="h-[15%] ml-4 mr-4 flex items-center">
+        <div className="h-[30%] w-[10%] flex flex-col justify-evenly justify ">
+            <div className="h-[10%] bg-soft-black "></div>
+            <div className="h-[10%] bg-soft-black"></div>
+            <div className="h-[10%] bg-soft-black"></div> 
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 
 import React from 'react'
+import NavBar from "../components/NavBar.jsx"
+import homeImg from "../assets/nails-holder_.jpeg"
 
 const Home = () => {
   return (
-   <div className="w-[100vw] h-[100vh] bg-off-white">
-    <div className="h-[5%] bg-burgundy">
+   <div className="w-screen h-screen bg-off-white overflow-scroll">
+    <NavBar/>
+    <div className="h-[5%] bg-burgundy flex items-center justify-center">
       <h2 className="text-off-white text-[0.75rem] font-[Merchant] font-extralight ">GET 10% OFF YOUR PURCHASE USE CODE NDMNLAUNCH</h2>
     </div>
+    <img src={homeImg} alt="Nails around a vinyl"/>
 
    </div>
   )
