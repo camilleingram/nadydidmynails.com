5fe61fbfda3087c8b1d709c37cff5699aedef414
camilleingram <camille_d_ingram@live.com>
2026-07-29 19:43:36 -0400

    set up nav bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 
+import cartIcon from "../assets/cart-icon.svg"
+
 const NavBar = () => {
   return (
-    <nav className="h-[15%] ml-4 mr-4 flex items-center">
-        <div className="h-[30%] w-[10%] flex flex-col justify-evenly justify ">
-            <div className="h-[10%] bg-soft-black "></div>
-            <div className="h-[10%] bg-soft-black"></div>
-            <div className="h-[10%] bg-soft-black"></div> 
+    <nav className="h-[10%] ml-4 mr-4 mb-5 flex items-end justify-between">
+        <div className="h-[18%] w-[8%] flex flex-col justify-between">
+            <div className="border-solid border-soft-black border-[1.2px] rounded-lg"></div>
+            <div className="border-solid border-soft-black border-[1.2px] rounded-lg"></div>
+            <div className="border-solid border-soft-black border-[1.2px] rounded-lg"></div>
         </div>
+        <h1 className="text-soft-black font-sugiyama text-[1.25rem]">nadydidmynails</h1>
+        <img src={cartIcon} alt="Shopping cart" className="w-[7%] stroke-soft-black"/>
     </nav>
   )
 }
 
 export default NavBar
+
+
