89c32fea5bac27a01740cd1ce284d4e4332a5e6c
camilleingram <camille_d_ingram@live.com>
2026-07-31 18:29:47 -0400

    finish step banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,16 +13,33 @@ const Home = () => {
     <img src={homeImg} alt="Nails around a vinyl" className="h-[85%] w-screen"/>
 
     {/* container of all numbers */}
-    <div className="h-[8%] bg-main-green pl-4 pr-4 overflow-x-hidden flex ">
+    <div className="h-[8%] w-screen bg-main-green flex items-center gap-[2rem]">
       {/* container of 1 number */}
-      <div className="w-[55%] h-[90%] flex flex-nowrap items-center justify-between">
+      <div className="w-[50%] h-[90%] flex shrink-0 items-center justify-between">
         <h3 className="font-[Merchant] text-off-white font-light">PICK YOUR SET</h3>
         {/* container of pill */}
         <div className="w-[40%] h-[55%] bg-off-white rounded-[50%] flex items-center justify-center ">
           <h4 className="font-[Merchant] text-main-green font-light">one</h4>
         </div>
       </div>
-
+      {/* container of 1 number */}
+      <div className="w-[44%] h-[90%] flex shrink-0 items-center justify-between">
+        <h3 className="font-[Merchant] text-off-white font-light">CUSTOMIZE</h3>
+        {/* container of pill */}
+        <div className="w-[46%] h-[55%] bg-off-white rounded-[50%] flex items-center justify-center ">
+          <h4 className="font-[Merchant] text-main-green font-light">two</h4>
+        </div>
+      </div>
+      {/* container of 1 number */}
+      <div className="w-[34%] h-[90%] flex shrink-0 items-center justify-between">
+        <h3 className="font-[Merchant] text-off-white font-light">APPLY</h3>
+        {/* container of pill */}
+        <div className="w-[59%] h-[55%] bg-off-white rounded-[50%] flex items-center justify-center ">
+          <h4 className="font-[Merchant] text-main-green font-light">three</h4>
+        </div>
+      </div>
+      
+   
     </div>
 
    </div>
