fadccd4424941b2c28df12da528a3f2fa9816ab8
camilleingram <camille_d_ingram@live.com>
2026-08-30 02:31:01 -0400

    adjust mailing list tablet viewport

--- frontend/src/components/MailingList.jsx
+++ frontend/src/components/MailingList.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const MailingList = () => {
   return (
     <>
-        <div className="justify-items-center self-center md:w-[40%]">
-           <h3 className="font-[Amoresa] text-off-white text-[2rem]">Be an Early Bird</h3>
-            <p className="font-[Merchant] font-extralight text-[0.75rem] text-center text-off-white w-[65%] md:w-[70%]">Be the first to know about exclusive deals and new releases by joining our mailing list</p> 
+        <div className="justify-items-center self-center md:w-[45%]">
+           <h3 className="font-[Amoresa] text-off-white text-[2rem] md:text-[2.125rem] ">Be an Early Bird</h3>
+            <p className="font-[Merchant] font-extralight text-[0.75rem] md:text-[1rem] text-center text-off-white w-[65%] md:w-[80%]">Be the first to know about exclusive deals and new releases by joining our mailing list</p> 
         </div>
         
         {/* add action and method attributes */}
-        <form name="mailing-list" className="flex flex-col justify-between gap-4 h-[35%] w-[70%] md:w-[40%] max-w-[21.7rem]">
+        <form name="mailing-list" className="flex flex-col justify-between gap-4 h-[35%] md:h-[45%] w-[70%] md:w-[40%] max-w-[21.7rem]">
             <label htmlFor="mailing-email" className="sr-only">Enter your email</label>
-            <input type="email" id="mailing-email" placeholder="Email" className="bg-off-white pr-2 pl-2 h-[40%] font-[Merchant] text-soft-black font-light text-[0.875rem] placeholder:text-soft-black placeholder:opacity-50 shrink-0" />
-            <button for="mailing-list" type="submit" className="bg-brown h-[40%] border-off-white border-2 rounded-[0.3125rem] font-[Merchant] text-off-white text-[0.875rem] text-center font-light shrink-0">Join now</button>
+            <input type="email" id="mailing-email" placeholder="Email" className="bg-off-white pr-2 pl-2 h-[40%] font-[Merchant] text-soft-black font-light text-[0.875rem] md:text-[1rem] placeholder:text-soft-black placeholder:opacity-50 shrink-0" />
+            <button for="mailing-list" type="submit" className="bg-brown h-[40%] border-off-white border-2 rounded-[0.3125rem] font-[Merchant] text-off-white text-[0.875rem] md:text-[1rem] text-center font-light shrink-0">Join now</button>
         </form>
     </>
   )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -49,10 +49,10 @@ const Home = () => {
       </div>
     </div>
 
-    <section className=" h-[40%] w-full ml-4 md:ml-5 mr-4 md:mr-5 mb-4 md:mb-5 relative z-0 flex flex-col justify-center">
-      <img src={hawaiianFlowerImg} alt="Hawaiian Flower" className=" h-auto w-[50%] max-w-68 absolute -translate-y-28 left-0 -translate-x-10 opacity-60 "/>
+    <section className=" h-[40%] md:h-[35%] w-full ml-4 md:ml-5 mr-4 md:mr-5 mb-4 md:mb-5 relative z-0 flex flex-col justify-center">
+      <img src={hawaiianFlowerImg} alt="Hawaiian Flower" className=" h-auto w-[50%] max-w-68 absolute -translate-y-20 left-0 -translate-x-10 opacity-60 "/>
       <h2 className="font-[Amoresa] text-[2rem] md:text-[2.25rem] absolute top-10 ">Shop by Collection</h2>
-      <div className="h-[60%] max-h-[55%] w-full flex gap-4 overflow-y-hidden z-10 shrink-0 absolute top-25 md:top-27 ">
+      <div className="h-[60%] w-full flex gap-4 overflow-y-hidden z-10 shrink-0 absolute top-25 md:top-27 ">
         <CollectionCard/>
         <CollectionCard/>
         <CollectionCard/>
@@ -60,9 +60,9 @@ const Home = () => {
         <CollectionCard/>
       </div>
       {/* <button type="button" className="bg-brown self-center shrink-0 h-[12%] w-[58%] max-w-57.5 md:w-[40%] font-[Merchant] font-bold text-off-white text-[1rem] rounded-md  z-10 absolute bottom-10 md:bottom-20 shadow-button">Shop the Collection</button> */}
-      <img src={dragonflyImg} alt="Dragonfly" className=" h-auto w-[60%] max-w-62 absolute bottom-0 right-0 translate-x-10 opacity-60 "/>
+      <img src={dragonflyImg} alt="Dragonfly" className=" h-auto w-[60%] max-w-68 absolute bottom-0 translate-y-10 right-0 translate-x-10 opacity-60 "/>
     </section>
-    <section className="h-[40%] w-full md:h-[35%] bg-nails bg-cover bg-clip-content bg-center ml-4 mr-4 mb-4 rounded-md shadow-button relative z-10 ">
+    <section className="h-[40%] w-full md:h-[35%] bg-nails bg-cover bg-clip-content bg-center ml-4 md:ml-5 mr-4 md:mr-5 mb-4 md:mb-5 rounded-md shadow-button relative z-10 ">
       <div className="h-[60%] w-[60%] flex flex-col items-end mt-4 absolute right-3">
         <h4 className="font-[Merchant] text-soft-black font-bold text-right text-[1.625rem] md:text-[2rem] text-shadow-[0_9px_4px_rgba(0_0_0/_0.7)]">HANDCRAFTED.</h4>
         <h4 className="font-[Merchant] text-soft-black font-bold text-right text-[1.625rem] md:text-[2rem] text-shadow-[0_9px_4px_rgba(0_0_0/_0.7)]">AFFORDABLE.</h4>
@@ -70,10 +70,10 @@ const Home = () => {
         <button type="button" className="h-[22%] w-[65%] max-w-46 bg-none border-soft-black border-2 rounded-md font-[Merchant] font-regular md:text-[1.25rem] text-center md:absolute md:bottom-3 ">Shop the looks</button>
       </div>
     </section>
-    <section className="h-[70%] sm:h-[50%] w-full mr-4 ml-4 mb-4 relative flex flex-col items-center">
+    <section className="h-[70%] sm:h-[50%] md:h-[45%] w-full mr-4 md:mr-5 ml-4 md:ml-5 mb-4 md:mb-5 relative flex flex-col items-center">
       <img src={longFlowerImg} alt="Long flower" className="h-auto w-[70%] max-w-68 absolute top-2 left-0 -translate-x-6 z-0 opacity-60"/>
       <h2 className="font-[Amoresa] text-[2rem] md:text-[2.25rem] absolute top-10 self-start">Nady's Favs</h2>
-      <div className="h-[80%] sm:h-[60%] w-[90%] max-w-126 sm:w-full sm:max-w-none absolute  top-28 z-10 grid grid-rows-2 grid-cols-2 gap-2 sm:flex sm:justify-left sm:gap-4 sm:overflow-x-auto">
+      <div className="h-[80%] sm:h-[60%] md:h-[65%] w-[90%] max-w-126 sm:w-full sm:max-w-none absolute  top-28 z-10 grid grid-rows-2 grid-cols-2 gap-2 sm:flex sm:justify-left sm:gap-4 sm:overflow-x-auto">
         <ItListCard/>
         <ItListCard/>
         <ItListCard/>
@@ -81,7 +81,7 @@ const Home = () => {
       </div>
       <img src={longFlowerImg} alt="Long flower" className="h-auto w-[60%] max-w-68 absolute top-5 right-0 translate-x-25 z-0 -rotate-17 opacity-60 "/>
     </section>
-    <section className="h-[35%] w-full  bg-main-green z-10 flex flex-wrap md:flex-row justify-center items-center md:gap-10 content-evenly rounded-[1.25rem]">
+    <section className="h-[35%] md:h-[25%] w-full  bg-main-green z-10 flex flex-wrap md:flex-row justify-center items-center md:gap-10 content-evenly rounded-[1.25rem]">
       <MailingList/>
     </section>
     <Footer/>
